708f48814adf808f3320856ffd1eac0971949fab
Rohan Pande <rohanpande@sanisoft.com>
2016-07-11 12:18:52 +0530

    change and completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import BookList from '../container/book-list';
+import BookDetail from '../container/book_detail';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+        <BookDetail />
+      </div>
     );
   }
 }
